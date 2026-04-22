0979a6b34f94e53c96ff38e81a17d5fde638d696
Diego <didipinheiro@outlook.com>
2026-04-21 21:43:19 -0300

    update base path for GitHub Actions deployment in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/chess-web/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/chatweb-frontend/' : '/',
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0'
