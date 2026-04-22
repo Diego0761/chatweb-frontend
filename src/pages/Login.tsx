e107529561e9e88b0a478517c83e857c0c844950
Diego <didipinheiro@outlook.com>
2026-04-21 23:02:50 -0300

    fix: remove unused Link component import in Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import api from '../utils/axios'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
 type AuthResponse = {
